98c3424f85b9448587d4942619b91f8a61c63681
Jaquelline <cjaquelliness@gmail.com>
2025-04-03 21:50:47 -0300

    Exercicios

---
+++ condicionais.js
@@ -0,0 +1,37 @@
+// //Basico 1//
+
+// let numero = prompt("Digite um número:") //promp é como eu solicito uma informação para o usuario//
+
+// if (numero > 0) {
+//     console.log("Positivo.");
+
+// } else {
+//     if (numero < 0) {
+//         console.log("Negativo");
+
+
+//     } else {
+//         console.log("É zero.");
+
+//     }
+// }
+
+
+
+
+//Basico 2//
+
+// let numero = prompt("Digite um numero")
+
+// if (numero % 2 === 0) {
+//     console.log("par");
+// } else {
+//     console.log("impar");
+// }
+
+
+// Basico 3//
+
+let numero = prompt("Insira sua idade:") 
+
+if (numero 0-12 >=) 
--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-alert ("Hello World na tela!");
+// alert ("Hello World na tela!");
 
-console.log ("Hello World no console!");
+// console.log ("Hello World no console!");
+
+let nome = "Jaquelline"; //string//
+const idade = 29; //Number// //não usamos "", se usarmos viraria uma string//
+var cidade = "São Paulo"; //string//
+
+console.log (nome);
+console.log (idade);
+console.log(cidade);
+
+
+console.log ("Meu nome é: " + nome);//concatenação//
+// console.log (´Tenho ${idade} anos´); //interpolação//
+console.log("Tenho " + idade + " anos");
+console.log("Minha cidade é: " + cidade ); 
+
+
+console.log (typeof idade); //decobrir o que é a variavel//
+
+let n1 = 10;
+let n2 = 10;
+let soma = n1 + n2;
+console.log(soma);
+
+
